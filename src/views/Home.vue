c77054f167bc03033fbe46b581f32acd1627a579
jean-michel-dot-fox <jean-michel@foxintelligence.fr>
2020-01-02 13:21:14 +0100

    feat(home): layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,38 @@
 <template>
   <div class="home">
-    welcome home
-    <v-btn to="/auth/sign-out">
-      sign out
-    </v-btn>
+    <div class="home__header">
+      <TheHomeHeader @onSignOut="handleSignOut" />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import { USER, SIGN_OUT } from '../store/modules/user-types';
+import TheHomeHeader from '../components/home/TheHomeHeader.vue';
+
 export default {
   name: 'Home',
+  components: { TheHomeHeader },
+  methods: {
+    ...mapActions(USER, {
+      signOut: SIGN_OUT,
+    }),
+    handleSignOut() {
+      this.signOut();
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  &__header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+}
+</style>
---
+++ src/views/periods/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="periods">
+    <h1>periods view</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Periods',
+};
+</script>
+
+<style lang="scss" scoped>
+.periods {
+    height: 100%;
+    display: flex;
+}
+</style>
