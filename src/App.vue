05bb773ba749cf98108084f4c25bea08d62a6dda
jean-michel-dot-fox <jean-michel@foxintelligence.fr>
2020-01-06 15:32:03 +0100

    fix(scrollbar): hide

--- src/App.vue
+++ src/App.vue
@@ -54,13 +54,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-body {
-  -ms-overflow-style: none;  /* Internet Explorer 10+ */
-    scrollbar-width: none;
-    ::-webkit-scrollbar {
-      display: none;  /* Safari and Chrome */
-    }
-}
-</style>
